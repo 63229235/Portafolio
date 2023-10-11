6ca8ab85e440a4716e96d32d857b4e592c31940c
Piero Revolledo <112639712+63229235@users.noreply.github.com>
2023-10-10 19:53:44 -0500

    commit

--- Practicas-Prog/Js/BizzBuzz.js
+++ Practicas-Prog/Js/BizzBuzz.js
@@ -3,14 +3,14 @@ let contador = 0;
 function FizzBuzz(n){
     while(n <= 100){
         n++;
-        if(n % 3 == 0){
-            console.log('Fizz');
+        if(n % 3 == 0 & n % 5 == 0){
+            console.log('FizzBuzz');
         }
         else if(n % 5 == 0){
             console.log('Buzz');
         }
-        else if(n % 3 == 0 & n % 5 == 0){
-            console.log('FizzBuzz');
+        else if(n % 3 == 0){
+            console.log('Fizz');
         }
         else{
             console.log(n);
